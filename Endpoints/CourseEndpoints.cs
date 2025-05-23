4589705fe1fabef2498f9f2c8eb8f98504507ebc
alp1n3 🌲 <59985265+alp1n3-dev@users.noreply.github.com>
2025-05-23 08:08:53 -0400

    checkpoint!

--- Endpoints/CourseEndpoints.cs
+++ Endpoints/CourseEndpoints.cs
@@ -7,10 +7,36 @@ public static class CourseEndpoints
     public static void AddCourseEndpoints(this WebApplication app)
     {
         app.MapGet("/courses", LoadAllCourses);
+        app.MapGet("/courses/{id}", LoadCourseById);
     }
 
-    private static IResult LoadAllCourses(CourseData data)
+    private static IResult LoadAllCourses(
+        CourseData data,
+        string? courseType,
+        string? search)
     {
-        return Results.Ok(data.Courses); 
+        var output = data.Courses;
+
+        if (string.IsNullOrEmpty(courseType) is false)
+        {
+            output.RemoveAll(x => string.Compare(
+                x.CourseType, 
+                courseType, 
+                StringComparison.OrdinalIgnoreCase) is not 0);
+        }
+        
+        return Results.Ok(output); 
+    }
+
+    private static IResult LoadCourseById(CourseData data, int id)
+    {
+        var output = data.Courses.SingleOrDefault(x => x.Id == id);
+
+        if (output is null)
+        {
+            return Results.NotFound();
+        }
+        
+        return Results.Ok(output);
     }
 }
